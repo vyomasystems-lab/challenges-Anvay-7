7915c14b02713d10d016861a15f7bdb04482e660
Anvay-7 <anvay10601@gmail.com>
2022-07-31 07:32:05 +0000

    corrected fsm code

---
+++ level1_design2_corrected/seq_detect_1011.v
@@ -0,0 +1,77 @@
+// See LICENSE.vyoma for more details
+// Verilog module for Sequence detection: 1011
+module seq_detect_1011(seq_seen, inp_bit, reset, clk);
+
+  output seq_seen;
+  input inp_bit;
+  input reset;
+  input clk;
+
+  parameter IDLE = 0,
+            SEQ_1 = 1, 
+            SEQ_10 = 2,
+            SEQ_101 = 3,
+            SEQ_1011 = 4;
+
+  reg [2:0] current_state, next_state;
+
+  // if the current state of the FSM has the sequence 1011, then the output is
+  // high
+  assign seq_seen = current_state == SEQ_1011 ? 1 : 0;
+
+  // state transition
+  always @(posedge clk)
+  begin
+    if(reset)
+    begin
+      current_state <= IDLE;
+    end
+    else
+    begin
+      current_state <= next_state;
+    end
+  end
+
+  // state transition based on the input and current state
+  always @(inp_bit or current_state)
+  begin
+    case(current_state)
+      IDLE:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_1;
+        else
+          next_state = IDLE;
+      end
+      SEQ_1:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_1; // Changed next state from IDLE to SEQ_1 as 1 followed by a 1 gives way to same state SEQ_1
+        else
+          next_state = SEQ_10;
+      end
+      SEQ_10:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_101;
+        else
+          next_state = IDLE;
+      end
+      SEQ_101:
+      begin
+        if(inp_bit == 1)
+          next_state = SEQ_1011;
+        else
+          next_state = SEQ_10; // Changed next state from IDLE to SEQ_10 as 101 followed by a 0 gives way to SEQ_10 state
+      end
+      SEQ_1011:
+      begin
+      // Changed the next state from IDLE for both 0 and 1 to SEQ_1 for 1011 followed by a 1
+        if(inp_bit == 1)
+          next_state = SEQ_1;
+        else
+          next_state = IDLE;
+      end
+    endcase
+  end
+endmodule
---
+++ level1_design2_corrected/test_seq_detect_1011.py
@@ -0,0 +1,63 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
+
+
+seq_states={"000":"IDLE","001":"SEQ_1","010":"SEQ_10","011":"SEQ_101","100":"SEQ_1011"}
+
+def get_state(pattern):
+    """Get the expected state value"""
+    if pattern[-4:]=="1011":
+        state="SEQ_1011"
+    elif pattern[-3:] == "101":
+        state="SEQ_101"
+    elif pattern[-2:]=="10":
+        state="SEQ_10"
+    elif pattern[-1:]=="1":
+        state="SEQ_1"
+    else:
+        state="IDLE"
+    return state
+
+@cocotb.test()
+async def test_seq_bug1(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    input_pattern="1010111011"
+    temp_seq=""
+    seq="1011"
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    # dut.inp_bit.value = 0 
+    await FallingEdge(dut.clk)
+    dut._log.info(f'Input bit = {dut.inp_bit.value} | DUT = {dut.seq_seen.value} | Current State = {dut.current_state.value} ({seq_states[str(dut.current_state.value)]})')
+    # cocotb.log.info('#### CTB: Develop your test here! ######')
+    for bit in input_pattern:
+        temp_seq+=bit
+        expected_state = get_state(temp_seq)
+        dut.inp_bit.value = int(bit)
+        await FallingEdge(dut.clk)
+        if temp_seq.find(seq)>-1:
+            output=1
+            temp_seq=""
+        else:
+            output=0
+
+        # print(dut.current_state.value)
+        current_state=str(dut.current_state.value)
+        dut._log.info(f'Input bit = {dut.inp_bit.value} | DUT = {dut.seq_seen.value} | Current State = {current_state} ({seq_states[current_state]})')
+
+        assert (output==int(dut.seq_seen.value)) & (expected_state==seq_states[current_state]) ,'\33[31m'+f"Expected DUT output = {output}, DUT value = {dut.seq_seen.value} | Expected DUT state = {expected_state}  DUT state = {seq_states[current_state]}"+'\x1b[0m'
