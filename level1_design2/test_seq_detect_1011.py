665ddfdea9a84ded031a94d84f6a2ff150135ea2
Anvay-7 <anvay10601@gmail.com>
2022-07-31 05:33:41 +0000

    expected state added

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -13,6 +13,19 @@ from cocotb.triggers import RisingEdge, FallingEdge
 
 seq_states={"000":"IDLE","001":"SEQ_1","010":"SEQ_10","011":"SEQ_101","100":"SEQ_1011"}
 
+def get_state(pattern):
+    if pattern[-4:]=="1011":
+        state="SEQ_1011"
+    elif pattern[-3:] == "101":
+        state="SEQ_101"
+    elif pattern[-2:]=="10":
+        state="SEQ_10"
+    elif pattern[-1:]=="1":
+        state="SEQ_1"
+    else:
+        state="IDLE"
+    return state
+
 @cocotb.test()
 async def test_seq_bug1(dut):
     """Test for seq detection """
@@ -29,10 +42,11 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     # dut.inp_bit.value = 0 
     await FallingEdge(dut.clk)
-    dut._log.info(f'Input bit = {dut.inp_bit.value} DUT = {dut.seq_seen.value} Current State = {dut.current_state.value} ({seq_states[str(dut.current_state.value)]})')
+    dut._log.info(f'Input bit = {dut.inp_bit.value} | DUT = {dut.seq_seen.value} | Current State = {dut.current_state.value} ({seq_states[str(dut.current_state.value)]})')
     # cocotb.log.info('#### CTB: Develop your test here! ######')
     for bit in input_pattern:
         temp_seq+=bit
+        expected_state = get_state(temp_seq)
         dut.inp_bit.value = int(bit)
         await FallingEdge(dut.clk)
         if temp_seq.find(seq)>-1:
@@ -42,6 +56,6 @@ async def test_seq_bug1(dut):
             output=0
 
         # print(dut.current_state.value)
-        dut._log.info(f'Input bit = {dut.inp_bit.value} DUT = {dut.seq_seen.value} Current State = {dut.current_state.value} ({seq_states[str(dut.current_state.value)]})')
+        dut._log.info(f'Input bit = {dut.inp_bit.value} | DUT = {dut.seq_seen.value} | Current State = {dut.current_state.value} ({seq_states[str(dut.current_state.value)]})')
 
-        assert output==int(dut.seq_seen.value),f"Output vale expected = {output}, DUT value = {dut.seq_seen.value}. Expected state = {} DUT state = {dut.current_state.value}"
+        assert output==int(dut.seq_seen.value),'\33[31m'+f"Expected DUT output = {output}, DUT value = {dut.seq_seen.value} | Expected DUT state = {expected_state}  DUT state = {seq_states[str(dut.current_state.value)]}"+'\x1b[0m'
